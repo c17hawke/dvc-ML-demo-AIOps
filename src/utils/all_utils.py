d8178cc948c904f5df847b360d3be2ca58457b35
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-26 09:32:20 +0530

    stage 01 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -7,3 +7,9 @@ def read_yaml(path_to_yaml: str) -> dict:
         content = yaml.safe_load(yaml_file)
 
     return content
+
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is created at {dir_path}")
